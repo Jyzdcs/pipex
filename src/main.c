a9a61c92c740345f3cfef24fa73ea9cec1e471cc
Jyzdcs <ky.claudant@gmail.com>
2025-03-08 19:58:23 +0100

    jsp

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: kclaudan <kclaudan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 16:21:00 by kclaudan          #+#    #+#             */
-/*   Updated: 2025/03/08 19:31:44 by kclaudan         ###   ########.fr       */
+/*   Updated: 2025/03/08 19:53:24 by kclaudan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,16 +50,16 @@ static int	setup_first_command(t_pipex *pipex, char *argv[], char *env[])
 {
 	int	fd;
 
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
-		handle_error("Error: Cannot open input file");
 	pipex->cmd_path = NULL;
 	if (is_absolute_path(argv[2]))
 	{
 		if (!process_absolute_path(argv[2], pipex))
 			handle_error("Error: Invalid absolute path");
 		if (access(pipex->cmd_path, F_OK | X_OK) == -1)
+		{
+			close_pipe(pipex->pipefd);
 			handle_error("Error: Command not found or not executable");
+		}
 	}
 	else
 	{
@@ -67,6 +67,9 @@ static int	setup_first_command(t_pipex *pipex, char *argv[], char *env[])
 		pipex->cmd_path = find_cmd_path(*pipex, env);
 		pipex->cmd_args = format_cmd(argv[2]);
 	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+		handle_error("Error: Cannot open input file");
 	return (fd);
 }
 
@@ -82,16 +85,16 @@ static int	setup_second_command(t_pipex *pipex, char *argv[], char *env[])
 {
 	int	fd;
 
-	fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd == -1)
-		handle_error("Error: Cannot open or create output file");
 	pipex->cmd_path = NULL;
 	if (is_absolute_path(argv[3]))
 	{
 		if (!process_absolute_path(argv[3], pipex))
 			handle_error("Error: Invalid absolute path");
 		if (access(pipex->cmd_path, F_OK | X_OK) == -1)
+		{
+			close_pipe(pipex->cmd_path);
 			handle_error("Error: Command not found or not executable");
+		}
 	}
 	else
 	{
@@ -99,6 +102,9 @@ static int	setup_second_command(t_pipex *pipex, char *argv[], char *env[])
 		pipex->cmd_path = find_cmd_path(*pipex, env);
 		pipex->cmd_args = format_cmd(argv[3]);
 	}
+	fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd == -1)
+		handle_error("Error: Cannot open or create output file");
 	return (fd);
 }
 
